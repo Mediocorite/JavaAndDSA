78a7a8a993089b58bf6810022be28560f6b9c255
Anzar Ahmad Malik <12586091+Mediocorite@users.noreply.github.com>
2025-07-23 14:00:58 +0100

    Intial Commit -> DSA Practice with understanding how public private work

---
+++ src/main/java/com/fundamental/datastructures/DynamicArray.java
@@ -0,0 +1,8 @@
+package com.fundamental.datastructures;
+
+
+public class DynamicArray {
+    public void testMethod() {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/fundamental/datastructures/Main.java
@@ -0,0 +1,9 @@
+package com.fundamental.datastructures;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DynamicArray test = new DynamicArray();
+        test.testMethod();
+    }
+}
